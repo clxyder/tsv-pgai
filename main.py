a9544e40b4f9ddfadb899b017c8fefda13f907ea
Carlos A. Wong <carloswong54@gmail.com>
2025-03-21 12:05:34 -0400

    feat: created functions for create_vectorizer and search

--- main.py
+++ main.py
@@ -15,47 +15,59 @@ engine = create_engine(Settings.db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 session = SessionLocal()
 
-# Create tables
-Base.metadata.create_all(bind=engine)
 
-vectorizer_statement = CreateVectorizer(
-    source="blog",
-    target_table='blog_embeddings',
-    embedding=EmbeddingLitellmConfig(
-        # model='openai/text-embedding-3-small',
-        model='ollama/nomic-embed-text',
-        dimensions=Settings.embedding_dim,
-    ),
-    chunking=ChunkingCharacterTextSplitterConfig(
-        chunk_column='content',
-        chunk_size=800,
-        chunk_overlap=400,
-        separator='.',
-        is_separator_regex=False
-    ),
-    formatting=FormattingPythonTemplateConfig(template='$title - $chunk')
-).to_sql()
 
-# TODO execute vectorizer_statement
+def create_vectorizer():
+    vectorizer_statement = CreateVectorizer(
+        source="blog",
+        target_table='blog_embeddings',
+        embedding=EmbeddingLitellmConfig(
+            model='ollama/nomic-embed-text',
+            dimensions=Settings.embedding_dim,
+        ),
+        chunking=ChunkingCharacterTextSplitterConfig(
+            chunk_column='content',
+            chunk_size=800,
+            chunk_overlap=400,
+            separator='.',
+            is_separator_regex=False
+        ),
+        formatting=FormattingPythonTemplateConfig(template='$title - $chunk')
+    ).to_sql()
 
-similar_posts = (
-    session.query(BlogPost.content_embeddings)
-    .order_by(
-        BlogPost.content_embeddings.embedding.cosine_distance(
-            func.ai.openai_embed(
-                "text-embedding-3-small",
-                "search query",
-                text("dimensions => 768")
+    # execute vectorizer_statement
+    r = session.execute(vectorizer_statement)
+    print(r)
+
+
+def search():
+    similar_posts = (
+        session.query(BlogPost.content_embeddings)
+        .order_by(
+            BlogPost.content_embeddings.embedding.cosine_distance(
+                func.ai.openai_embed(
+                    "text-embedding-3-small",
+                    "search query",
+                    text("dimensions => 768")
+                )
             )
         )
+        .limit(5)
+        .all()
     )
-    .limit(5)
-    .all()
-)
+    
+    print(similar_posts)
+
 
 def main():
     print("Hello from tsv-pgai!")
+    # Create tables
+    Base.metadata.create_all(bind=engine)
+    
+    # create vectorizer
+    create_vectorizer()
 
 
 if __name__ == "__main__":
     main()
+    
